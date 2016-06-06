22b4b4164cb137b2f8627b8d4dee88975165cb77
c3t3m3 <chentongming@gmail.com>
2016-06-07 01:03:09 +0800

    shell

---
+++ restart.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+docker stop rabbitmq-server && docker rm rabbitmq-server
+docker run -d --name=rabbitmq-server -p 5672:5672 -p 15672:15672 -e RABBITMQ_DEFAULT_USER=rabbit -e RABBITMQ_DEFAULT_PASS=mq_passwd c3t3m3/docker-rabbitmq
